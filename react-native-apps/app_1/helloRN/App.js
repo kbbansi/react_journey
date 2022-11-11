e35a4eba1114c76eb01327d4d2405aa69a3f7df1
kbbansi <kwabenaampofo5@gmail.com>
2022-11-11 09:46:11 +0000

    enhance basic layout with flex-box

--- react-native-apps/app_1/helloRN/App.js
+++ react-native-apps/app_1/helloRN/App.js
@@ -4,12 +4,17 @@ import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Enter your course goals'/>
+      
+      <View style={styles.appTitle}>
+        <Text>Goal Tracker</Text>
+      </View>
+      
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Enter your course goals'/>
         <Button title="Add Goal"/>
       </View>
 
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of Goals...</Text>
       </View>
     </View>
@@ -18,6 +23,39 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1
+  }, 
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cfcccf',
+    
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5
+  },
+  //my own component, not part of course
+  appTitle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    fontSize: 10,
+    fontWeight: '200',
+    borderWidth: 1,
+    borderColor: '#eeeeee',
+    alignItems: 'center',
+    backgroundColor: '#dddfec'
   }
 });

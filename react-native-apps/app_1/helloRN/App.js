199811be18ef679b5c1929b2a28e7b270cafd4ad
kbbansi <kwabenaampofo5@gmail.com>
2022-11-11 08:42:18 +0000

    add simple layout

--- react-native-apps/app_1/helloRN/App.js
+++ react-native-apps/app_1/helloRN/App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='Enter your course goals'/>
+        <Button title="Add Goal"/>
+      </View>
+
+      <View>
+        <Text>List of Goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  appContainer: {
+    padding: 50
+  }
 });
